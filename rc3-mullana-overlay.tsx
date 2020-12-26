d0b77880c029f4852b07d147313133a02d8a2525
hashinana <67550402+hashinana@users.noreply.github.com>
2020-12-26 23:11:47 +0100

    test

---
+++ rc3-mullana-overlay.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="rc3-mullana-overlay" tilewidth="32" tileheight="32" tilecount="512" columns="16">
+ <editorsettings>
+  <export target="." format=""/>
+ </editorsettings>
+ <image source="rc3-mullana-overlay.png" width="512" height="1024"/>
+</tileset>
---
+++ test1.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="test1" tilewidth="32" tileheight="32" tilecount="768" columns="32">
+ <editorsettings>
+  <export target="." format=""/>
+ </editorsettings>
+ <image source="test3.png" width="1024" height="768"/>
+</tileset>
