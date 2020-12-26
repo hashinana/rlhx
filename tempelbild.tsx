cc9f36281c149195c0786c87635df36cfad682ef
hashinana <67550402+hashinana@users.noreply.github.com>
2020-12-26 23:29:54 +0100

    datenpfade

---
+++ tempelbild.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tempel" tilewidth="32" tileheight="32" tilecount="551" columns="29">
+ <editorsettings>
+  <export target="." format=""/>
+ </editorsettings>
+ <image source="IMG_0059.png" width="950" height="634"/>
+</tileset>
--- zelt.tsx
+++ zelt.tsx
@@ -1,7 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="zelt" tilewidth="32" tileheight="32" tilecount="15" columns="5">
- <editorsettings>
-  <export target="." format=""/>
- </editorsettings>
  <image source="2_Rundzelt_0.png" width="160" height="96"/>
 </tileset>
