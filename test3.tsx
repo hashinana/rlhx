da412488fc3301dd26a8b6552f2b8f2077355f81
hashinana <67550402+hashinana@users.noreply.github.com>
2020-12-26 22:50:57 +0100

    pfade neu

--- test3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="test3" tilewidth="32" tileheight="32" tilecount="768" columns="32">
- <image source="../../../Desktop/RLHx/test3.jpg" width="1024" height="768"/>
-</tileset>
